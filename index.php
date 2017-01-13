ff88694a7090e2bf145bfc238d7c639f9f248540
joefhall <joefhall@gmail.com>
2017-01-13 15:14:33 +0000

    Add initial files

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+// Show errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+header('Content-type: text/html; charset=utf-8');
+
+// Stop user's browser from caching, so only latest version of page loaded
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header('Content-type: text/html; charset=utf-8');
+
+?>
+
+
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+
+    <meta charset="utf-8" />
+    <title>Loan Shark Banks</title>
+    <meta property="og:title" content="Loan Shark Banks" />
+    <meta property="og:description" content="blah blah blah............" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://something........" />
+    <link rel="canonical" href="http://something........">
+    <meta property="og:image" content="http://image................" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Bevan|Nunito+Sans" rel="stylesheet">
+
+    <!-- JQuery -->
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    
+    <!-- JQuery UI -- for Autocomplete -->
+    <script src="jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="jquery-ui.min.css" />
+
+    <!-- Recaptcha used to check person isn't a bot -->
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+		
+		<!-- Unslider -->
+		<script src="unslider/unslider-min.js"></script>
+		<script src="//stephband.info/jquery.event.move/js/jquery.event.move.js"></script>
+		<script src="//stephband.info/jquery.event.swipe/js/jquery.event.swipe.js"></script>
+		<link rel="stylesheet" href="unslider/unslider.css" />
+		<link rel="stylesheet" href="unslider/unslider-dots.css" />
+
+    <!-- Google Analytics -- add later -->
+		
+		<link rel="stylesheet" href="/styles.css">
+
+  </head>
+
+  <body>
+		
+		<div id="workinprogress">
+			Hi! Welcome to this <strong>work-in-progress development site.</strong> Please bear in mind there's lots more functionality, better design, better messaging and all to be added, and bug fixes to do.
+		</div>
+    
+    <div id="pagecontent">
+
+      <div id="maincontent">
+
+        <h1>
+          Loan shark banks are...
+        </h1>
+
+
+        
+
+      </div>
+
+    </div>
+  
+    
+    <script>
+      
+      
+      
+    </script>
+
+  </body>
+
+
+  </html>
